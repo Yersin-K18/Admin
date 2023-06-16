22d51a72e83809450e28cd5553dce87bd08b81f2
phu <phu3365@gmail,com>
2023-06-16 18:29:52 +0700

    directive_index

--- app/directive/directive.js
+++ app/directive/directive.js
@@ -1,6 +1,18 @@
-app.directive('loginPage', ()=> {
+app.directive('loginpage', () => {
     return {
-        restrict: 'E',
-        templateURL: './login/login.html',
-    }
-})
+      restrict: 'E',
+      templateUrl: './app/directive/login/login.html',
+      controller: () => {
+        console.log('load login');
+      },
+    };
+  });
+  app.directive('sidebar', () => {
+    return {
+      restrict: 'E',
+      templateUrl: '/app/directive/index/sidebar.html',
+      controller: () => {
+        console.log('load sidebar');
+      },
+    };
+  });
--- app/route.js
+++ app/route.js
@@ -5,3 +5,10 @@ function config($routeProvider) {
       template: '<login-page></login-page>'
     })
 }]);
+app.config(['$routeProvider',
+function config($routeProvider) {
+  $routeProvider.
+    when('/', {
+      templateUrl:'/app/directive/index/content.html',
+    })
+}]);
